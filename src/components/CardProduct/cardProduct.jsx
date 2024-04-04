bc897f9ade89fa5189a22a80ea639ec49677c1c3
Dev-One <jeiferleon@gmail.com>
2024-04-03 20:53:08 -0500

    Cambios de altura en el CardProduct y cambio de titulo en los productos

--- src/components/CardProduct/cardProduct.jsx
+++ src/components/CardProduct/cardProduct.jsx
@@ -4,15 +4,19 @@ import './cardProduct.css';
 function cardProduct({title, category, image, description, price}) {
   return (
     <div className='card-div'>
-        <div className='card-title'>
-          <p>{title}</p>
-          <a href={`/${category}`}>{category}</a>
+        <div className='card-content'>
+          <div className='card-title'>
+            <p>{title}</p>
+            <a href={`/${category}`}>{category}</a>
+          </div>
+          <div className='card-img'>
+            <img src={image} alt={title} />
+          </div>
+          <div className='card-information'>
+            <p>Description: <span>{description}</span></p>
+          </div>  
         </div>
-        <div className='card-img'>
-          <img src={image} alt={title} />
-        </div>
-        <div className='card-information'>
-          <p>Description: <span>{description}</span></p>
+        <div className='card-price'>
           <p>Price: <span>${price}</span></p>
         </div>
     </div>
--- src/context/CatalogueContext/index.jsx
+++ src/context/CatalogueContext/index.jsx
@@ -5,37 +5,37 @@ const CatalogueContext = React.createContext();
 function CatalogueProvider({ children }) {
 
     const products = [{
-        title: "Bolso Mujer",
+        title: "Bolso Mujer 1",
         category: "woman",
         image: "https://belcorpcolombia.vtexassets.com/arquivos/ids/936140-800-800?v=638296381698600000&width=800&height=800&aspect=true",
         price: 123,
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel impedit quaerat commodi necessitatibus accusantium, rerum maiores quos aliquam id fugit sint cupiditate placeat repudiandae ipsam, velit error magnam voluptatum esse.",
     },{
-        title: "Bolso Hombre",
+        title: "Bolso Hombre 1",
         category: "man",
         image: "https://exitocol.vtexassets.com/arquivos/ids/5294284/hombres-bolso-de-lujo-canguro-marca-cuero-messenger-bolso-hombre-bolsa.jpg?v=637432976293500000",
         price: 456,
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel impedit quaerat commodi necessitatibus accusantium",
     },{
-        title: "Bolso Niño",
+        title: "Bolso Niño 1",
         category: "kids",
         image: "https://i.ebayimg.com/thumbs/images/g/x-cAAOSwH~pldsvm/s-l640.jpg",
         price: 789,
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel impedit quaerat commodi necessitatibus accusantium, rerum maiores quos aliquam id fugit sint cupiditate",
     },{
-        title: "Bolso Hombre",
+        title: "Bolso Hombre 2",
         category: "man",
         image: "https://exitocol.vtexassets.com/arquivos/ids/5294284/hombres-bolso-de-lujo-canguro-marca-cuero-messenger-bolso-hombre-bolsa.jpg?v=637432976293500000",
         price: "987",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel impedit quaerat commodi necessitatibus accusantium, rerum maiores quos aliquam id fugit sint cupiditate",
     },{
-        title: "Bolso Niños",
+        title: "Bolso Niños 2",
         category: "kids",
         image: "https://i.ebayimg.com/thumbs/images/g/x-cAAOSwH~pldsvm/s-l640.jpg",
         price: "654",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel impedit quaerat commodi necessitatibus accusantium, rerum maiores quos aliquam id fugit sint cupiditate placeat repudiandae ipsam, velit error magnam voluptatum esse.",
     },{
-        title: "Bolso Mujer",
+        title: "Bolso Mujer 2",
         category: "woman",
         image: "https://belcorpcolombia.vtexassets.com/arquivos/ids/936140-800-800?v=638296381698600000&width=800&height=800&aspect=true",
         price: "321",
